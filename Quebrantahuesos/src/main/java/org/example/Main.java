aa224549ee298c8dd477f22c1989ee80dbe89ae9
JulioJuarez <240072@iessanalberto.com>
2025-06-16 19:44:52 +0200

    Agregando nuevas funcionalidades al programa

--- Quebrantahuesos/src/main/java/org/example/Main.java
+++ Quebrantahuesos/src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.example.Models.Ciclista;
+import org.example.Models.Equipo;
 import org.example.Repository.Repositorio;
 import org.example.Services.CarreraService;
-
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -14,29 +16,49 @@ public class Main {
 
             Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Bienvenido al Simulador de Carreras de Ciclismo");
-            System.out.println("Selecciona una carrera para comenzar:");
-            System.out.println("1. Quebrantahuesos");
-            System.out.println("2. Treparriscos");
-            System.out.println("3. Pirenaica");
-            System.out.print("Opción: ");
+        int opcion = 0;
+        while (opcion !=4){
+            System.out.println("1.- Mostrar Equipos");
+            System.out.println("2.- Mostrar Ciclistas");
+            System.out.println("3.- Simular Carrera");
+            System.out.println("4.- Salir");
+            System.out.println("Introduce opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion){
+                case 1 -> {
+                    List<Equipo> equipos = repositorio.getEquipos();
+                    for(Equipo equipo : equipos){
+                        System.out.println(equipo);
+                    }
+                }
+                case 2 -> {
+                    List<Ciclista> ciclistas = repositorio.getCiclistas();
+                    for(Ciclista ciclista : ciclistas){
+                        System.out.println(ciclista);
+                    }
+                }
+                case 3 -> {
+                    System.out.println("Bienvenido al Simulador de Carreras de Ciclismo");
+                    System.out.println("Selecciona una carrera para comenzar:");
+                    System.out.println("1. Quebrantahuesos");
+                    System.out.println("2. Treparriscos");
+                    System.out.println("3. Pirenaica");
+                    System.out.print("Opción: ");
 
-            int opcion = scanner.nextInt();
+                    int opcionCarrera = scanner.nextInt();
 
-            switch (opcion) {
-                case 1:
-                    carreraService.simularCarrera(repositorio.getCarreras().get(0));
-                    break;
-                case 2:
-                    carreraService.simularCarrera(repositorio.getCarreras().get(1));
-                    break;
-                case 3:
-                    carreraService.simularCarrera(repositorio.getCarreras().get(2));
-                    break;
-                default:
-                    System.out.println("Opción no válida");
+                    switch (opcionCarrera) {
+                        case 1 -> carreraService.simularCarrera(repositorio.getCarreras().get(0));
+                        case 2 -> carreraService.simularCarrera(repositorio.getCarreras().get(1));
+                        case 3 -> carreraService.simularCarrera(repositorio.getCarreras().get(2));
+                        default -> System.out.println("Opción no válida");
+                    }
+                }
+                case 4 -> System.out.println("Fin de programa");
+                default -> System.out.println("Opción no contemplada");
             }
-
+        }
             scanner.close();
     }
 
